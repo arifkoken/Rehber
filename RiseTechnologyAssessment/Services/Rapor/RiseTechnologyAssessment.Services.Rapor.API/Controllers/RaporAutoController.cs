e277670fe379e376b601b2f1009b3113d2f4e244
Arif KÖKEN <arif_koken@hotmail.com>
2021-10-10 17:57:03 +0300

    Endpointler için ön hazırlık yapıldı.(Controller ve  Actionlar oluşturuldu. parametreler ve dönüş türleri belirlendi.)

---
+++ RiseTechnologyAssessment/Services/Rapor/RiseTechnologyAssessment.Services.Rapor.API/Controllers/RaporAutoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RiseTechnologyAssessment.Services.Rapor.API.Models.Dto;
+
+namespace RiseTechnologyAssessment.Services.Rapor.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RaporAutoController : ControllerBase
+    {
+        [HttpPost]
+        public void Talep([FromBody] RaporTalepDto model)
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<RaporListDto> Listele()
+        {
+            return new List<RaporListDto>() { };
+        }
+
+
+        [HttpGet("{id}")]
+        public RaporDetayDto Detay(int id)
+        {
+            return new RaporDetayDto() { };
+        }
+
+    }
+}
---
+++ RiseTechnologyAssessment/Services/Rehber/RiseTechnologyAssessment.Services.Rehber.API/Controllers/KisiController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RiseTechnologyAssessment.Services.Rehber.API.Models.Dto;
+
+namespace RiseTechnologyAssessment.Services.Rehber.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class KisiController : ControllerBase
+    {
+
+        [HttpPost]
+        public void Ekle([FromBody] KisiOlusturDto model)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Sil(int id)
+        {
+
+        }
+
+        [HttpPost]
+        public void IletisimBilgisiEkle([FromBody] KisiEkBilgiOlusturDto model)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void IletisimBilgisiSil(int id)
+        {
+
+        }
+
+
+        [HttpGet]
+        public IEnumerable<KisiListDto> Listele()
+        {
+            return new List<KisiListDto>() { };
+        }
+
+
+        [HttpGet("{id}")]
+        public KisiDetayDto Detay(int id)
+        {
+            return new KisiDetayDto() { };
+        }
+
+    }
+}
