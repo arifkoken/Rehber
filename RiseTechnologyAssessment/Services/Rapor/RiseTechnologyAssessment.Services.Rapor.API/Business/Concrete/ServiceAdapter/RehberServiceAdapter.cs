210a26088dd902d4edc6659761251ab58e0600a4
Arif KÖKEN <arif_koken@hotmail.com>
2021-10-12 20:54:45 +0300

    İstek yapılan url bilgisi appsettings'den çekilecek şekilde ayarlandı.

--- RiseTechnologyAssessment/Services/Rapor/RiseTechnologyAssessment.Services.Rapor.API/Business/Concrete/ServiceAdapter/RehberServiceAdapter.cs
+++ RiseTechnologyAssessment/Services/Rapor/RiseTechnologyAssessment.Services.Rapor.API/Business/Concrete/ServiceAdapter/RehberServiceAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using RiseTechnologyAssessment.Services.Rapor.API.Business.Abstract.ServiceAdapter;
 using RiseTechnologyAssessment.Services.Rapor.API.Models.Dto;
@@ -9,12 +10,16 @@ namespace RiseTechnologyAssessment.Services.Rapor.API.Business.Concrete.ServiceA
 {
     public class RehberServiceAdapter : IRehberServiceAdapter
     {
+        private IConfiguration _configuration;
+        public RehberServiceAdapter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public async Task<KonumaGoreRaporDto> KonumaGoreRapor(int raporId, int konumId)
         {
             var client = new HttpClient();
             var response =
-                await client.GetAsync("http://localhost:31089/api/Kisi/KonumaGoreRehberVerileri/" + raporId + "/" +
-                                      konumId);
+                await client.GetAsync(_configuration["RehberKonumDetayEndPointAdres"] + raporId + "/" + konumId);
             var content = await response.Content.ReadAsStringAsync();
             var data = JsonConvert.DeserializeObject<KonumaGoreRaporDto>(content);
             return new KonumaGoreRaporDto() { RaporId = raporId, TopKisiSayisi = data.TopKisiSayisi, ToplamTelefonNoSayisi = data.ToplamTelefonNoSayisi };
